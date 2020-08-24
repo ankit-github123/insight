6de31f15d3dd91ba88ee95c4addaa46d231b6e99
ankit-github123 <ankit190900@gmail.com>
2020-08-24 20:14:59 +0530

    my second commit insight

---
+++ build/precache-manifest.c47a8e302c9230dc6cea854199ee6d65.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec3283399255dccd3db920de54ba8df7",
+    "url": "/insight/index.html"
+  },
+  {
+    "revision": "2a6ae3b24e0eb9aa2a57",
+    "url": "/insight/static/css/2.8278ec6d.chunk.css"
+  },
+  {
+    "revision": "53bdf855d0fc22a1cdd3",
+    "url": "/insight/static/css/main.37868d64.chunk.css"
+  },
+  {
+    "revision": "2a6ae3b24e0eb9aa2a57",
+    "url": "/insight/static/js/2.0227e51f.chunk.js"
+  },
+  {
+    "revision": "53bdf855d0fc22a1cdd3",
+    "url": "/insight/static/js/main.4dbdab0e.chunk.js"
+  },
+  {
+    "revision": "a15121beddd3a2cb498d",
+    "url": "/insight/static/js/runtime-main.e0bc964c.js"
+  },
+  {
+    "revision": "10b8b845af874a5ea5b01df6d6abecb5",
+    "url": "/insight/static/media/Capture.10b8b845.PNG"
+  },
+  {
+    "revision": "d1062ba101d7ee0d31a8feecc5bcb49c",
+    "url": "/insight/static/media/about.d1062ba1.jpg"
+  },
+  {
+    "revision": "94727c30845681da7da7f920567403a9",
+    "url": "/insight/static/media/blog.94727c30.jpg"
+  },
+  {
+    "revision": "4526fa2c4f378e4a66be8bd49bfc2177",
+    "url": "/insight/static/media/happy.4526fa2c.jpg"
+  },
+  {
+    "revision": "037d3991ae159bd0d7c3e7ab3eafccba",
+    "url": "/insight/static/media/laugh-start.037d3991.gif"
+  },
+  {
+    "revision": "ef97be54bea0538affbf33a55a3a0c22",
+    "url": "/insight/static/media/laughing.ef97be54.gif"
+  },
+  {
+    "revision": "8275c3078d8ba9fb1145fe716b650543",
+    "url": "/insight/static/media/leftsign.8275c307.PNG"
+  },
+  {
+    "revision": "b247fde7e719a84c368c25ca6e175e81",
+    "url": "/insight/static/media/news.b247fde7.png"
+  },
+  {
+    "revision": "02fb91daab755f61478c1d1935e7790c",
+    "url": "/insight/static/media/rightsign.02fb91da.PNG"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/insight/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/insight/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/insight/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/insight/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "8a15ffd365be606e398d3f0872b8f71f",
+    "url": "/insight/static/media/trivia-start.8a15ffd3.jpg"
+  },
+  {
+    "revision": "b8f031416b8f5db62f735c70f75365da",
+    "url": "/insight/static/media/writer-start.b8f03141.gif"
+  },
+  {
+    "revision": "d41f02a46fc2346cbde83b592ebb4aca",
+    "url": "/insight/static/media/writer.d41f02a4.jpg"
+  },
+  {
+    "revision": "483160e7f03c9eb0d6cdaeebc8d116bb",
+    "url": "/insight/static/media/youtube.483160e7.jpg"
+  }
+]);
